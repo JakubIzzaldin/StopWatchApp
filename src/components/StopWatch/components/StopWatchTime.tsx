71cac128a6085646529b0d1c66b0a2922e1720c3
JakubIzzaldin <81328425+JakubIzzaldin@users.noreply.github.com>
2024-03-23 13:11:33 +0100

    feat(JI) StopWatchTime component

---
+++ src/components/StopWatch/components/StopWatchTime.tsx
@@ -0,0 +1,31 @@
+import {HStack, Text} from '@chakra-ui/react';
+
+export type StopWatchTimeProps = {
+  hour: number;
+  minute: number;
+  second: number;
+  milliseconds: number;
+  separator?: string;
+};
+
+export const StopWatchTime = ({
+  hour,
+  minute,
+  second,
+  separator,
+  milliseconds,
+}: StopWatchTimeProps) => {
+  const formatTime = (time: number) => time.toString().padStart(2, '0');
+
+  return (
+    <HStack gap={1}>
+      <Text>{formatTime(hour)}</Text>
+      <Text fontSize={'large'}>{separator}</Text>
+      <Text fontSize={'large'}>{formatTime(minute)}</Text>
+      <Text fontSize={'large'}>{separator}</Text>
+      <Text fontSize={'large'}>{formatTime(second)}</Text>
+      {milliseconds && <Text fontSize={'large'}>{separator}</Text>}
+      {milliseconds && <Text fontSize={'large'}>{formatTime(milliseconds)}</Text>}
+    </HStack>
+  );
+};
